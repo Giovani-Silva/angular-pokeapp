0e93a17142611b434a1a2b51a5e97cf661b6276c
Giovani Silva <giovani.silva@live.com>
2020-07-13 21:24:07 -0300

    change server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-// Start the app by listening on the default Heroku port
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+
 app.listen(process.env.PORT || 8080);
